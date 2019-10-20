1ce9966ccce58e7c320de6defad3ebaaf6dd9d2e
Chris Waweru <chriswaweru70@gmail.com>
2019-10-20 23:02:29 +0300

    Numbers

---
+++ chapter_2/sandbox.js
@@ -0,0 +1,18 @@
+let r = 10;
+const pi = 3.142;
+
+let Area = pi * r ** 2;
+
+// console.log(Area);
+
+//math operators +, -, *, /, **, %
+
+// console.log(10 ** 2);
+
+// let result = (37 * 3);
+
+//order of opertion - B I D M A S
+
+let result = 5 * (10 - 3) ** 2;
+
+console.log(result);
