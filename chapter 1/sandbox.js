b5df7f4dac6053a4be06ceea3d88e06aaa9996f9
Chris Waweru <chriswaweru70@gmail.com>
2019-10-20 17:42:16 +0300

    Common String Methods

--- chapter 1/sandbox.js
+++ chapter 1/sandbox.js
@@ -1,20 +1,59 @@
+let a = "55";
+let b = a - 5;
+let c = a + 5;
+console.log(b + ":" + c);
 
-let a = 4;
-let b = 90;
+// let b = 90;
 
-a = 40;
+// a = 40;
 
-let ans = a * b;
+// let ans = a * b;
 
 
-console.log(ans)
+// console.log(ans)
 
 //strings
+console.log('Chris Kamau');
 
+let email = "chriswaweru70@gmail.com";
+
+console.log(email);
 //string concatenation
+
+var FirstName = 'Chenzy';
+
+var LastName = 'Weng';
+
+let FullName = FirstName + ' ' + LastName;
+
+console.log(FullName);
 
 //getting characters
 
+console.log(FullName[7])
+
 //string length
 
+console.log(FullName.length)
+
 //string methods
+
+console.log(FullName.toUpperCase());
+let result = FirstName.toLowerCase();
+console.log(result);
+
+let index = email.indexOf('@');
+console.log(index);
+
+let indexp = email.slice(4, 10);
+
+let indexc = email.substr(4, 10);
+
+let inch = email.replace('h', '');
+
+let push = inch.replace('c', 'k');
+console.log(indexp);
+
+console.log(indexc);
+
+console.log(push);
