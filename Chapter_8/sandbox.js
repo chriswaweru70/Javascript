159e4963587481ac14333d811daf59e8d78e12d6
Chris Waweru <chriswaweru70@gmail.com>
2019-10-23 19:56:04 +0300

    For Loop

---
+++ Chapter_8/sandbox.js
@@ -0,0 +1,14 @@
+// for loop
+
+// for (i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+// console.log('done')
+
+let names = ['BG', 'Kiprop', 'Dennis', 'Alfred', 'Chris'];
+
+for (i = 0; i < names.length; i++) {
+    // console.log(names[i]);
+    let html = `<div>${names[i]}</div>`
+    console.log(html)
+}
