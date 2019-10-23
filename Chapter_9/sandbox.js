f06deac5bd49f6b88514f82a90db2388414237ea
Chris Waweru <chriswaweru70@gmail.com>
2019-10-23 20:36:17 +0300

    While and do while

---
+++ Chapter_9/sandbox.js
@@ -0,0 +1,24 @@
+// White loop
+
+let i = 0;
+
+while (i < 5) {
+    console.log('loop is', i)
+    i++
+}
+
+let i = 0;
+
+const names = ['Arya', 'John', 'Snow', 'Targarians', 'pishipishi', 'riririr']
+while (i < names.length) {
+    console.log('This is is', names[i])
+    i++
+}
+
+// Do while loops
+
+let i = 3;
+do {
+    console.log('This is it', i);
+    i++
+} while (i < 5);
