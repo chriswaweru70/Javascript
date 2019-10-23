a7b0be73886abf0fa19ce0fa22e2b4a14aa141d9
Chris Waweru <chriswaweru70@gmail.com>
2019-10-23 16:08:30 +0300

    Strict Comparison and loose Comparison

---
+++ Chapter_5/sandbox.js
@@ -0,0 +1,37 @@
+let age = null;
+
+console.log(age, age + 3, `let the value of his age be ${age}`);
+
+Booleans and Comparisons
+
+console.log(true, false, "true", "false");
+
+methods can return booleans
+
+let myEmail = 'chriswaweru70@gmail.com';
+let name = ['ishi', 'fooshy', 'Rishy'];
+
+
+let result = myEmail.includes('@');
+let inch = name.includes('ishi');
+
+console.log(inch);
+
+Comparison operators
+let age = 30
+
+console.log(age == 30);
+console.log(age == 25);
+console.log(age != 80)
+console.log(age > 24);
+console.log(age < 20);
+console.log(age <= 30);
+console.log(age >= 30);
+
+let name = 'chris';
+
+console.log(name == 'chris');
+console.log(name == 'Chris');
+console.log(name > 'absolom');
+console.log(name > 'aelipe');
+
---
+++ Chapter_6/sandbox.js
@@ -0,0 +1,15 @@
+let age = 25;
+
+// loose Comparison (different types can still be equal)
+
+console.log(age == 25);
+console.log(age == '25');
+console.log(age != 25);
+console.log(age != '25');
+
+// strict comparison (different types cannot be equal)
+
+console.log(age === 25);
+console.log(age === '25');
+console.log(age !== 25);
+console.log(age !== '25')
