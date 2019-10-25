af7c027e3400224cfd69bdc2dd99df210436fdf5
Chris Waweru <chriswaweru70@gmail.com>
2019-10-25 12:20:43 +0300

    If statement

---
+++ Chapter_10/sandbox.js
@@ -0,0 +1,19 @@
+// If statements
+
+let age = 30;
+
+if (age > 39) {
+    console.log('This is greater than 30');
+}
+
+const ninjas = ['Shaun', 'Ryu', 'Chris', 'Fish', 'Gilbert']
+
+if (ninjas.length > 4) {
+    console.log('So many ninjas');
+}
+
+const password = 'pass'
+
+if (password.length < 8) {
+    console.log('This is a weak password')
+}
